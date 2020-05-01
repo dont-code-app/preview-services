7884848ee3cd0c755d9aad2f74a9a8d49dc5d8c1
Gérard Collin <gerard.collin@allianz.com>
2020-05-01 17:58:47 +0200

    Working services

---
+++ src/main/java/org/dontcode/preview/FromIdeResource.java
@@ -0,0 +1,27 @@
+package org.dontcode.preview;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/updates")
+public class FromIdeResource {
+    private static Logger log = LoggerFactory.getLogger(PreviewSocket.class);
+
+    @Inject
+    protected PreviewSocket preview;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response receiveUpdate (String update) {
+        log.info("From IDE"+update);
+        preview.broadcast(update);
+        return Response.ok().build();
+    }
+}
---
+++ src/main/java/org/dontcode/preview/PreviewSocket.java
@@ -0,0 +1,55 @@
+package org.dontcode.preview;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/preview")
+@ApplicationScoped
+public class PreviewSocket {
+    private static Logger log = LoggerFactory.getLogger(PreviewSocket.class);
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        log.info("Session opened");
+        sessions.put(String.valueOf(System.currentTimeMillis()), session);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        log.info("Session closed");
+        String key[] = new String[1];
+        sessions.entrySet().forEach(stringSessionEntry -> {
+            if (stringSessionEntry.getValue()==session)
+                key[0]=stringSessionEntry.getKey();
+        });
+        sessions.remove(key[0]);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        log.info("Message: "+ message);
+        //broadcast(">> " + message);
+    }
+
+    public void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result ->  {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+}
