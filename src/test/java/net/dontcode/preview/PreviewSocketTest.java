735fa56b4bc4616de0c869346b465e3dc7c56888
Gérard Collin <gerard.collin@appliweb.net>
2021-06-08 18:47:02 +0200

    Fixed unit tests

--- src/test/java/net/dontcode/preview/PreviewSocketTest.java
+++ src/test/java/net/dontcode/preview/PreviewSocketTest.java
@@ -37,8 +37,12 @@ public class PreviewSocketTest extends AbstractMongoTest {
     @Inject
     SessionService sessionService;
 
+    public static String firstSessionId= UUID.randomUUID().toString();
+
     @Test
     public void testSession() throws DeploymentException, IOException, InterruptedException {
+        ClientTestSession.receivedMessages.clear();
+        ClientTestSession.opened=false;
         try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(ClientTestSession.class, uri)) {
             // Wait the data to be saved in the database
             for (int i = 0; i < 10; i++) {
@@ -62,8 +66,8 @@ public class PreviewSocketTest extends AbstractMongoTest {
                     .statusCode(HttpStatus.SC_OK);
 
             Thread.sleep(50);
-            // Then send a message for another previewer (sessionId is different)
-            toSend = new Message(Message.MessageType.CHANGE, "123" ,chg);
+            // Then send a message for out previewer
+            toSend = new Message(Message.MessageType.CHANGE, firstSessionId ,chg);
 
             given()
                     .contentType(ContentType.JSON)
@@ -79,20 +83,21 @@ public class PreviewSocketTest extends AbstractMongoTest {
                     break;
                 }
             }
-            Assertions.assertEquals(ClientTestSession.receivedMessages.size(),1, "No Messages broadcasted or Message was not filtered per sesssionId");
+            Assertions.assertEquals(1, ClientTestSession.receivedMessages.size(), "No Messages broadcasted or Message was not filtered per sesssionId");
         }
     }
 
-    public static String sessionId= UUID.randomUUID().toString();
+    public static String otherSessionId = UUID.randomUUID().toString();
 
     @Test
     public void testLatePreview() throws DeploymentException, IOException, InterruptedException {
+        OtherClientTestSession.receivedMessages.clear();
+        OtherClientTestSession.opened=false;
         // First create sessions with a specific sessionId
         Change chg = new Change(Change.ChangeType.RESET, "/", null);
-        Message toSend = new Message(Message.MessageType.CHANGE, sessionId, chg);
 
-        sessionService.createNewSession(sessionId, "Test Session").await().indefinitely();
-        sessionService.updateSession(sessionId, chg).await().indefinitely();
+        sessionService.createNewSession(otherSessionId, "Test Session").await().indefinitely();
+        sessionService.updateSession(otherSessionId, chg).await().indefinitely();
 
         var value = new JsonObject("""
                 {
@@ -100,11 +105,11 @@ public class PreviewSocketTest extends AbstractMongoTest {
                 }""").toBsonDocument();
         var pointer = new DontCodeModelPointer("creation/entities/a", "creation/entities","creation", "creation", null, "a");
         chg = new Change(Change.ChangeType.RESET, "creation/entities/a", value, pointer);
-        sessionService.updateSession(sessionId, chg).await().indefinitely();
+        sessionService.updateSession(otherSessionId, chg).await().indefinitely();
 
         pointer = new DontCodeModelPointer("creation/entities/a/name", "creation/entities/name","creation/entities/a", "creation/entities", "name", null);
         chg = new Change(Change.ChangeType.ADD, "creation/entities/a/name", "NewNameA", pointer);
-        sessionService.updateSession(sessionId, chg).await().indefinitely();
+        sessionService.updateSession(otherSessionId, chg).await().indefinitely();
 
         value = new JsonObject("""
                 {
@@ -119,19 +124,20 @@ public class PreviewSocketTest extends AbstractMongoTest {
                 }""").toBsonDocument();
         pointer = new DontCodeModelPointer("creation/entities/a/fields", "creation/entities/fields","creation/entities/a", "creation/entities", "fields", null);
         chg = new Change(Change.ChangeType.RESET, "creation/entities/a/fields", value, pointer);
-        sessionService.updateSession(sessionId, chg).await().indefinitely();
+        sessionService.updateSession(otherSessionId, chg).await().indefinitely();
 
+        OtherClientTestSession.opened=false;
         // Then connect the preview and it should receive all of the updates
-        try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(ClientTestSession.class, uri)) {
+        try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(OtherClientTestSession.class, uri)) {
             // Wait the data to be saved in the database
             for (int i = 0; i < 10; i++) {
                 Thread.sleep(50);
-                if( ClientTestSession.receivedMessages.size()>3) {
+                if( OtherClientTestSession.receivedMessages.size()>3) {
                     break;
                 }
             }
-            Assertions.assertTrue(ClientTestSession.opened, "Session was not opened");
-            Assertions.assertEquals(4, ClientTestSession.receivedMessages.size(), "Not all previous messages received");
+            Assertions.assertTrue(OtherClientTestSession.opened, "Session was not opened");
+            Assertions.assertEquals(4, OtherClientTestSession.receivedMessages.size(), "Not all previous messages received");
         }
     }
 
@@ -140,13 +146,18 @@ public class PreviewSocketTest extends AbstractMongoTest {
 
         public static boolean opened=false;
         public static List<Message> receivedMessages = new Vector<>();
+        protected String sessionId;
+
+        public ClientTestSession () {
+          sessionId=firstSessionId;
+        }
 
         @OnOpen
         public void open(Session session) {
             //MESSAGES.add("CONNECT");
             // Send a message to indicate that we are ready,
             // as the message handler may not be registered immediately after this callback.
-            session.getAsyncRemote().sendObject(new Message (Message.MessageType.INIT, sessionId));
+            session.getAsyncRemote().sendObject(new Message (Message.MessageType.INIT,sessionId));
             opened=true;
         }
 
@@ -162,6 +173,14 @@ public class PreviewSocketTest extends AbstractMongoTest {
             System.err.println("Error "+ error.getMessage());
         }
 
+    }
+
+    @ClientEndpoint(encoders = MessageEncoderDecoder.class, decoders = MessageEncoderDecoder.class)
+    public static class OtherClientTestSession extends ClientTestSession {
+        public OtherClientTestSession () {
+            super();
+            sessionId = otherSessionId;
+        }
     }
 
 
